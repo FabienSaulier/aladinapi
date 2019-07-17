883c769e099b36e569c6bdeaafc51de850d22a68
Fabien Saulier <fsaulier@gmail.com>
2019-07-17 16:30:24 +0200

    handle amazon

--- url.py
+++ url.py
@@ -1,8 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from flask_restful import Resource
 from bs4 import BeautifulSoup
 import json
 from flask import  request
 import requests
+import time
 
 class Url(Resource):
 
@@ -10,49 +13,46 @@ class Url(Resource):
     url = request.args.get('url')
     return scrape(url)
 
-
-
-  def getBeautifulSoup_viaBrowser(url):
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox(options=options, executable_path=r'./geckodriver')
-    driver.set_window_size(1120, 850)
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-
+def getBeautifulSoup_viaBrowser(url, delay=0):
+  options = Options()
+  options.headless = True
+  driver = webdriver.Firefox(options=options, executable_path=r'./geckodriver')
+  driver.set_window_size(1120, 850)
+  driver.get(url)
+  time.sleep(delay)
+  soup = BeautifulSoup(driver.page_source, 'html.parser')
+  driver.quit()
+  return soup
 
 def getBeautifulSoup_viaGet(url):
   response = requests.get(url)
   return BeautifulSoup(response.text, 'html.parser')
 
-
 def scrapeCastorama(url):
   soup = getBeautifulSoup(url)
-  title = soup.find("meta", property="og:title")
-  url = soup.find("meta", property="og:url")
-  image = soup.find("meta", property="og:image")
-  description = soup.find("meta", attrs={'name': "description"})
-  price = soup.find("meta", itemprop="price")
-  priceCurrency = soup.find("meta", itemprop="priceCurrency")
+  the_title = soup.find("meta", property="og:title").get('content')
+  the_url = soup.find("meta", property="og:url").get('content')
+  the_image = soup.find("meta", property="og:image").get('content')
+  the_description = soup.find("meta", attrs={'name': "description"}).get('content')
+  the_price = soup.find("meta", itemprop="price").get('content')
+  the_priceCurrency = soup.find("meta", itemprop="priceCurrency").get('content')
 
   data = {
-    'label': title.get('content'),
-    'url': url.get('content'),
+    'label': the_title,
+    'url': the_url,
     'images': [
-      image.get('content')
+      the_image
     ],
     'price': {
-      'amount': price.get('content'),
-      'currency': priceCurrency.get('content')
+      'amount': the_price,
+      'currency': the_priceCurrency
     },
     'extra_data': {
-      'description': description.get('content')
+      'description': the_description
     }
   }
 
   return data
-
 
 def scrapeLeroyMerlin(url):
   soup = getBeautifulSoup(url)
@@ -66,33 +66,75 @@ def scrapeLeroyMerlin(url):
 
   data = None
   if product is not None:
+    the_title = product['name']
+    the_url = product['url']
+    the_image = product['image']
+    the_description = product['description']
+    the_price = product['offers']['price']
+    the_priceCurrency = product['offers']['priceCurrency']
     data = {
-      'label': product['name'],
-      'url': product['url'],
+      'label': the_title,
+      'url': the_url,
       'images': [
-        product['image']
+        the_image
       ],
       'price': {
-        'amount': product['offers']['price'],
-        'currency': product['offers']['priceCurrency']
+        'amount': the_price,
+        'currency': the_priceCurrency
       },
       'extra_data': {
-        'description': product['description']
+        'description': the_description
       }
     }
   return data
 
+def scrapeAmazon(url):
+  soup = getBeautifulSoup(url)
+  data_metrics = soup.find("div", id="cerberus-data-metrics")
+
+  the_price = data_metrics.get('data-asin-price')
+  the_priceCurrency = data_metrics.get('data-asin-currency-code')
+  the_description = soup.find("meta", attrs={'name': 'description'}).get('content')
+  the_title = soup.find("meta", attrs={'name': 'title'}).get('content')
+  the_url = url
+  the_image = soup.find("img", id="landingImage").get('src')
+
+  data = {
+    'label': the_title,
+    'url': the_url,
+    'images': [
+      the_image
+    ],
+    'price': {
+      'amount': the_price,
+      'currency': the_priceCurrency
+    },
+    'extra_data': {
+      'description': the_description
+    }
+  }
+
+  return data
 
 def getBeautifulSoup(url):
   return getBeautifulSoup_viaGet(url)
 
-
 def scrape(url):
-  data = None
+  data = {
+    "error": "This website is not yet covered by scraping"
+  }
   if 'castorama.fr' in url:
     data = scrapeCastorama(url)
   elif 'leroymerlin.fr' in url:
     data = scrapeLeroyMerlin(url)
-  elif 'leroymerlin.es' in url:
-    data = scrapeLeroyMerlin(url)
+  elif 'amazon.fr' in url:
+    data = scrapeAmazon(url)
+  elif 'amazon.com' in url:
+    data = scrapeAmazon(url)
+  # LMES doesn't work :
+  # - no "og:image"      meta tag
+  # - no "price"         meta tag
+  # - no "priceCurrency" meta tag
+  #    elif 'leroymerlin.es'            in url:
+  #        data = scrapeCastorama(url)
   return data
